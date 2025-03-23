ea02c1bf7bfdfb308cbae25c65d6e458cc42578c
pari-ranasaria28 <pari.ranasaria@gmail.com>
2025-03-23 16:18:18 +0530

    Updated the connection file to host it

--- includes/connect.php
+++ includes/connect.php
@@ -1,9 +1,9 @@
 <?php
 // $con=mysqli_connect('localhost','root','','shopnest');
-$servername = "sql306.infinityfree.com"; // Your database hostname
-$username = "if0_38266918";  // Your database username
-$password = "VrvhwGOx1e8";  // Your database password
-$dbname = "if0_38266918_shopnest";  // Your database name
+$servername = "sql306.infinityfree.com"; // database hostname
+$username = "if0_38266918";  // database username
+$password = "VrvhwGOx1e8";  // database password
+$dbname = "if0_38266918_shopnest";  // database name
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
